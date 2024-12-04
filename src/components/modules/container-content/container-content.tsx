e59184abc811bf1eea646bd08b648606e2669baf
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-12-04 08:18:51 -0400

    feature: create container container-content

---
+++ src/components/modules/container-content/container-content.tsx
@@ -0,0 +1,30 @@
+import FooterContent from "../footer/footer-content";
+import MainContent from "../main/main-content";
+
+const ContainerContent = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <main
+      role='main'
+      id='container-content'
+      className={`
+        relative
+        mt-[4rem]
+        w-full
+        h-screen
+        flex
+        flex-col
+        min-w-screen
+        `}
+    >
+      <MainContent children={children} />
+
+      <FooterContent />
+    </main>
+  );
+};
+
+export default ContainerContent;
