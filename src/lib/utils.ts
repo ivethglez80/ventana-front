5c6df987ab1e4eab5619769633134196b58a1165
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-12-07 18:44:30 -0400

    wip: added validate url for map

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function isValidGoogleMapsUrl(url: string): boolean {
+  const googleMapsRegex = /^https:\/\/www\.google\.com\/maps\/embed\?/;
+  return googleMapsRegex.test(url);
+}
