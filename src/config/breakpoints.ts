6213f8d89ffc6e782b3f2a52d903b2b249ef3e5b
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-12-07 10:16:58 -0400

    wip: split breakpoints for responsive

--- src/config/breakpoints.ts
+++ src/config/breakpoints.ts
@@ -1,15 +1,9 @@
-export const BREAKPOINTS = {
+import { Breakpoint, BreakpointConfig } from "./interfaces/breakpoints-interface";
+
+export const BREAKPOINTS: Record<Breakpoint, BreakpointConfig> = {
   xs: { max: 639 },
   sm: { min: 640, max: 767 },
   md: { min: 768, max: 1023 },
   lg: { min: 1024, max: 1279 },
   xl: { min: 1280 },
-};
-
-export const getBreakpoint = (width: number) => {
-  if (width <= BREAKPOINTS.xs.max) return 'xs';
-  if (width >= BREAKPOINTS.sm.min && width <= BREAKPOINTS.sm.max) return 'sm';
-  if (width >= BREAKPOINTS.md.min && width <= BREAKPOINTS.md.max) return 'md';
-  if (width >= BREAKPOINTS.lg.min && width <= BREAKPOINTS.lg.max) return 'lg';
-  return 'xl';
 };
