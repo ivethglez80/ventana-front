f235d629a7206117433a20c2268b251f595b9007
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-12-09 10:40:21 -0400

    feature: create container for reviews, newsletter and cards-reservation

---
+++ src/components/modules/wrapper-main/wrapper-main.tsx
@@ -0,0 +1,22 @@
+import { Newsletter } from '@/components/newsletter/newsletter';
+import { Reviews } from '@/components/reviews/reviews';
+
+interface WrapperMainProps {
+  children?: React.ReactNode;
+  className?: string;
+}
+
+export const WrapperMain: React.FC<WrapperMainProps> = ({
+  children,
+  className= ''
+}) => {
+  return (
+    <div className={`flex justify-center mt-10 lg:bg-[#f0f0f0] ${className}`}>
+      <div className='lg:w-7/12 h-fit flex flex-col items-center w-full'>
+        <Reviews />
+        <Newsletter />
+        {children}
+      </div>
+    </div>
+  );
+};
