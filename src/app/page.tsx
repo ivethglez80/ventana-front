71d2d08656193a1abca604e34cb5fc499e716ccf
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-12-09 15:33:21 -0400

    wip: added shows

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import { Services } from '@/components/services/services';
-import {ContactInfo} from '@/components/contact-info/contact-info'
+import { ContactInfo } from '@/components/contact-info/contact-info';
 import { GoogleMap } from '@/components/map/map-location';
 import { WrapperMain } from '@/components/modules/wrapper-main/wrapper-main';
-import {Consultas} from '@/components/contact-form/consultas';
+import { Consultas } from '@/components/contact-form/consultas';
+import { Shows } from '@/components/shows/shows';
 const Home = () => {
   return (
     <main
@@ -13,8 +14,9 @@ const Home = () => {
     `}
     >
       {/* features */}
+      <Shows />
       <WrapperMain />
-      <Consultas/>
+      <Consultas />
       <GoogleMap />
       <ContactInfo />
       <Services />
