dfcd20c48ee6605306aa9a4df2d5c698ac97a8f3
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-12-09 09:38:21 -0400

    wip: added newsletter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Services } from '@/components/services/services';
 import {ContactInfo} from '@/components/contact-info/contact-info'
 import { GoogleMap } from '@/components/map/map-location';
+import { Newsletter } from '@/components/newsletter/newsletter';
 const Home = () => {
   return (
     <main
@@ -11,8 +12,13 @@ const Home = () => {
     `}
     >
       {/* features */}
-      <GoogleMap/>
-      <ContactInfo/>
+      <div className='flex justify-center'>
+        <div className='lg:w-7/12 h-fit flex flex-col items-center w-full'>
+          <Newsletter />
+        </div>
+      </div>
+      <GoogleMap />
+      <ContactInfo />
       <Services />
     </main>
   );
