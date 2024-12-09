766f889e0d0969b6c2db127492f8b8f675a3e643
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-12-09 09:36:35 -0400

    feature: component newsletter

---
+++ src/components/newsletter/newsletter-form.tsx
@@ -0,0 +1,89 @@
+import { Button, Input } from '@nextui-org/react';
+import { useForm, Controller } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { toast } from 'sonner';
+import { useValidation } from '@/hooks/useValidation';
+import { useNewsletter } from '@/actions/newsletter/hooks/useNewsletter';
+import type { NewsletterSchemaType } from '@/actions/newsletter/types/newsletter.types';
+import { newsletterSchema } from '@/actions/newsletter/schemas/newsletter-schema';
+export const NewsletterForm = () => {
+  const { validate } = useValidation(newsletterSchema);
+  const { subscribe, loading } = useNewsletter();
+
+  const {
+    control,
+    handleSubmit,
+    watch,
+    reset,
+    formState: { errors },
+  } = useForm<NewsletterSchemaType>({
+    resolver: zodResolver(newsletterSchema),
+    defaultValues: {
+      email: '',
+      source: 'web',
+    },
+  });
+
+  const onSubmit = async (data: NewsletterSchemaType) => {
+    const validationResult = validate(data);
+
+    if ('errors' in validationResult) {
+      console.error('Validation errors:', validationResult.errors);
+      return;
+    }
+
+    const success = await subscribe(data.email);
+    if (success) {
+      reset();
+    }
+  };
+
+  const onError = (errors: Record<string, any>) => {
+    if (errors.email) {
+      console.log(errors);
+      toast.error(`${errors.email.message}`);
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit, onError)}
+      acceptCharset='UTF-8'
+      id='frmSuscribir'
+      role='form'
+      className='w-full flex gap-2 items-baseline'
+      noValidate={true}
+    >
+      <Controller
+        name='email'
+        control={control}
+        render={({ field }) => (
+          <Input
+            {...field}
+            type='email'
+            label='Email'
+            errorMessage={errors.email?.message}
+            isInvalid={!!errors.email}
+            classNames={{
+              base: 'border-gray-900 animate-delay-400 focus:border-gray-300',
+            }}
+            labelPlacement='inside'
+            variant='underlined'
+            required
+            className='block placeholder-black py-2.5 px-4 w-full text-sm text-gray-900 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-black-500 focus:outline-none focus:ring-0 peer mt-0'
+          />
+        )}
+      />
+
+      <div className='flex items-end'>
+        <Button
+          type='submit'
+          variant='solid'
+          className='mb-2 text-black uppercase w-1/2 lg:w-10/12 focus:outline-none rounded-lg text-sm lg:text-lg px-5 py-2 mt-1 text-center bg-pallete-gold focus:bg-gold-light'
+        >
+          Enviar
+        </Button>
+      </div>
+    </form>
+  );
+};
---
+++ src/components/newsletter/newsletter.tsx
@@ -0,0 +1,66 @@
+'use client';
+
+import { fontMontserrat, fontRubik } from '@/fonts/fonts';
+import Image from 'next/image';
+import { NewsletterForm } from './newsletter-form';
+import { Toaster } from 'sonner';
+import { ErrorBoundary } from '../errors/error-boundary';
+
+export const Newsletter: React.FC = () => {
+  return (
+    <section
+      id='suscribe'
+      className={`w-full bg-[#f0f0f0] mt-10 border-t-1 border-black sm:border-none lg:bg-white lg:bg-transparent flex justify-center py-4 md:mx-0 md:min-h-[100px] ${fontMontserrat.className} antialiased`}
+    >
+      <Toaster richColors />
+
+      <div
+        className={`
+          w-full
+        lg:bg-white
+        sm:rounded-lg
+        lg:rounded-l-lg
+        sm:bg-transparent
+        lg:shadow
+        `}
+      >
+        <div className='w-full flex flex-col items-center lg:flex-row'>
+          {/* first box */}
+          <div
+            className={`w-full lg:basis-1/2 flex flex-col justify-center items-center ${fontRubik.className} tracking-wide`}
+          >
+            {/* title */}
+            <h4
+              className={`font-bold tracking-wide uppercase text-base mt-2 md:text-xl`}
+            >
+              suscribite a nuestro newsletter
+            </h4>
+
+            {/* form */}
+            <div className='flex mt-0 sm:px-10 space-x-0 border-6 w-full'>
+              <ErrorBoundary>
+                <NewsletterForm />
+              </ErrorBoundary>
+            </div>
+          </div>
+
+          {/* flag - second box */}
+          <div className='hidden lg:basis-1/2 lg:flex min-h-[150px]'>
+            <div className='relative'>
+              <Image
+                data-src='https://www.laventanaweb.com/images/argentina-newsletter.jpg'
+                alt='Argentina flag'
+                className='h-full object-cover'
+                src='https://www.laventanaweb.com/images/argentina-newsletter.jpg'
+                priority={true}
+                quality={100}
+                width={900}
+                height={800}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
