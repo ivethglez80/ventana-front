f6ca890bc970b4abee3c46452631f9d3fc74532d
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-12-10 01:02:04 -0400

    wip: added reservations

--- src/components/modules/wrapper-main/wrapper-main.tsx
+++ src/components/modules/wrapper-main/wrapper-main.tsx
@@ -1,4 +1,5 @@
 import { Newsletter } from '@/components/newsletter/newsletter';
+import { Reservations } from '@/components/reservations/reservations';
 import { Reviews } from '@/components/reviews/reviews';
 
 interface WrapperMainProps {
@@ -13,6 +14,7 @@ export const WrapperMain: React.FC<WrapperMainProps> = ({
   return (
     <div className={`flex justify-center lg:bg-[#f0f0f0] ${className}`}>
       <div className='lg:w-7/12 h-fit flex flex-col items-center w-full'>
+        <Reservations/>
         <Reviews />
         <Newsletter />
         {children}
