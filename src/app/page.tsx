a86b4b085b3350a3544c55bef687eac4a2719385
Francisco A. Myers M. <franciscomyers@gmail.com>
2024-12-10 01:50:48 -0400

    wip: added vimeo-video

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,9 @@ import { MenuGallery } from '@/components/menu/menu-gallery';
 import { Anexo } from '@/components/anexo/anexo';
 import { Salones } from '@/components/salones/salones';
 import { Search } from '@/components/search/search';
+import { DynamicVimeoHome } from '@/components/vimeo/dynamic-vimeo';
 const Home = () => {
+  
   return (
     <main
       id='home'
@@ -18,6 +20,7 @@ const Home = () => {
     `}
     >
       {/* features */}
+      <DynamicVimeoHome/>
       <Salones/>
       <Search/>
       <Anexo/>
